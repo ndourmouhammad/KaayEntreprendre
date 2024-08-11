89abca43a83f1f41b37e244958288a55936c86aa
ndourmouhammad <ndourmouhammad@gmail.com>
2024-08-11 08:58:42 +0000

    corrections de la route

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,13 @@
-<?php
+
 
 use App\Http\Controllers\CommentaireController;
 use App\Http\Controllers\DiscussionController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\ReservationController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum')
-->apiResource('reservations', ReservationController::class);
+
+
+Route::apiResource('reservations', ReservationController::class);
 Route::apiResource('discussions', DiscussionController::class);
 Route::get('notifications', [NotificationController::class, 'index'])->name('notifications.index');
 Route::get('discussions/search', [DiscussionController::class, 'recherche'])->name('discussions.search');
